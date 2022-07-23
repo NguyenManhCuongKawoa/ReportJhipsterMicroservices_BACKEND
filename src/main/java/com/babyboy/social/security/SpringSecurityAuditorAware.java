543b8b8144472fbee21b76ac977f6a50bc9b8751
CuongNM <cuongnm@aladintech.co>
2022-07-23 21:34:34 +0700

    Initial version of backendsocial generated by generator-jhipster@7.6.0

---
+++ src/main/java/com/babyboy/social/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.babyboy.social.security;
+
+import com.babyboy.social.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
